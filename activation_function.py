45c6dc7db9fbdebae2dcea06637ec974957d1d83
takiyama0617 <tuto.tuto.0617@gmail.com>
2020-07-08 20:58:29 +0900

    Deep Learning
    Chap3 シグモイド関数

--- activation_function.py
+++ activation_function.py
@@ -12,3 +12,12 @@ y = step_function(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
 plt.show()
+
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x))
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1,1)
+plt.show()
