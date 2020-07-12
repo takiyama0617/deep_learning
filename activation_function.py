05e731c868a336e1dfdacc5506bd449268f979af
takiyama0617 <tuto.tuto.0617@gmail.com>
2020-07-12 13:38:50 +0900

    Deep Learning
    Chap3 出力層の設計

--- activation_function.py
+++ activation_function.py
@@ -54,3 +54,22 @@ network = init_network()
 x = np.array([1.0, 0.5])
 y = forward(network, x)
 print(y)
+
+#
+# 出力層の設計
+# 一般的に
+#  回帰問題→恒等関数
+#  分類問題→ソフトマックス関数
+
+def softmax(a):
+  c = np.max(a)
+  # オーバーフロー対策
+  exp_a = np.exp(a - c)
+  sum_exp_a = np.sum(exp_a)
+  y = exp_a / sum_exp_a
+  return y
+
+a = np.array([0.3, 2.9, 4.0])
+y = softmax(a)
+print(y)
+print(np.sum(y))
