800600d24345bc85d62baf3732067a6d5ab38fe2
takiyama0617 <tuto.tuto.0617@gmail.com>
2020-08-03 23:29:41 +0900

    Deep learning
    Chap4.5.2 ミニバッチの実装

---
+++ train_neuralnet.py
@@ -0,0 +1,44 @@
+import numpy as np
+from dataset.mnist import load_mnist
+from two_layer_net import TwoLayerNet
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+train_loss_list = []
+
+iters_num = 100#10000
+train_size = x_train.shape[0]
+batch_size = 1000
+learning_rate = 0.1
+
+train_acc_list = []
+test_acc_list = []
+iter_per_epoch = max(train_size / batch_size, 1)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+    print('progress ...' + str(i))
+    # ミニバッチ取得
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # 勾配の計算
+    grad = network.numerical_gradient(x_batch, t_batch)
+
+    # パラメータ更新
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] = learning_rate * grad[key]
+    
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", ", + str(test_acc))
+
+
